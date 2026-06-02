7efe7c1943fc5df35fc5d77732b6598a4ed711e1
Shobhit Gupta <shobhitgupta828@gmail.com>
2026-06-02 23:08:34 +0530

    Updated the Validation for the input by the user

--- src/service/StudentService.java
+++ src/service/StudentService.java
@@ -5,10 +5,48 @@ import java.util.ArrayList;
 public class StudentService {
     ArrayList<Student> studentList = new ArrayList<>();
 
+    public boolean isStudentExists(int id){
+        for(Student student : studentList){
+            if(student.getId() == id){
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean isStudentValid(Student student){
+        if(student.getAge()<=16 || student.getAge()>=60){
+            System.out.println("Enter a valid Age between 16-60");
+            return false;
+        }
+        else if(student.getMarks()>100 || student.getMarks()<0){
+            System.out.println("Enter the valid Marks between 0-100");
+            return false;
+        }
+        else if(student.getEmail() == null || !student.getEmail().contains("@")){
+            System.out.println("Enter a valid Email that contains @");
+            return false;
+        }
+        else if(student.getId()<=0){
+            System.out.println("Enter the valid Id - greater than 0");
+            return false;
+        }
+        else if(student.getName() == null || student.getName().trim().isEmpty()){
+            System.out.println("Enter the valid Name");
+            return false;
+        }
+        else
+            return true;
+    }
     public void addStudent(Student student){
-        studentList.add(student);
-        System.out.println("Student added successfully");
-        System.out.println();
+        if(isStudentExists(student.getId())){
+            System.out.println("Student already exists with this Id");
+            System.out.println();
+        }
+        else if(isStudentValid(student)) {
+            studentList.add(student);
+            System.out.println("Student added successfully");
+            System.out.println();
+        }
     }
     public void viewStudent(){
         if(studentList.isEmpty()) {
